d3d1a3783c960c643d511cacd1388e438a8e7943
juancronos <juandavid.torres@gmail.com>
2017-08-25 16:46:34 -0500

    Por medio de NG-route, se agrego navegacion dentro de la app, un dashboard y la lista de todos los heroes.

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,11 +2,11 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
+  { id: 12, name: 'Chapulin' },
+  { id: 13, name: 'Bombastic' },
   { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
+  { id: 16, name: 'ChocoKrispis' },
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
